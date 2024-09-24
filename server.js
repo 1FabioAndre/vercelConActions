1585985d423b71e0d37e7c7c9993c8c2cd3c9c45
FabioMojica <fabioandremojicaarmaza@gmail.com>
2024-09-24 16:24:46 -0400

    Despliegue

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(morgan('dev'));
 // Rutas
 app.get('/', (req, res) => {
   //res.json({ message: 'Hola desde tu API en Vercel!' });
-  res.json({ message: '¡Hola! produccion este es un cambio' });
+  res.json({ message: '¡Hola! development este es un gran cambio' });
 });
 
 app.get('/saludo', (req, res) => {
